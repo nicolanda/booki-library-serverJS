0b13a28cff6969d9a23ee0b16c557f2a0bd2d2c8
nicolanda <nicolaslandazabal@hotmail.com>
2022-10-19 14:50:12 -0500

    rawData added

---
+++ src/services/rawData.js
@@ -0,0 +1,7 @@
+export const documentTypeValues = [
+  { name: 'Cédula de ciudadanía' },
+  { name: 'Cédula de extranjería' },
+  { name: 'Pasaporte' },
+  { name: 'Tarjeta de identidad' },
+  { name: 'NIT' }
+];
